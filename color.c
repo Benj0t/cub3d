0276f09107ed89ef237d364b89d1bfcfea805488
Benjamin MOREAU <bemoreau@student.42.fr>
2020-06-25 19:53:46 +0200

    This is moving

---
+++ color.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bemoreau <bemoreau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/25 15:48:12 by bemoreau          #+#    #+#             */
+/*   Updated: 2020/06/25 18:54:14 by bemoreau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+// /!\ 6 functions
+
+t_color ft_color_divide(t_color color)
+{
+	t_color new;
+	new.R = (color.R) ? color.R / 2 : color.R;
+	new.G = (color.G) ? color.G / 2 : color.G;
+	new.B = (color.B) ? color.B / 2 : color.B;
+	return (new);
+}
+
+t_color	ft_light_blue()
+{
+	t_color color;
+
+	color.R = 53;
+	color.G = 155;
+	color.B = 255;
+	return (color);
+}
+
+t_color	ft_gray()
+{
+	t_color color;
+
+	color.R = 128;
+	color.G = 128;
+	color.B = 128;
+	return (color);
+}
+
+t_color	ft_red()
+{
+	t_color color;
+
+	color.R = 255;
+	color.G = 0;
+	color.B = 0;
+	return (color);
+}
+
+t_color	ft_green()
+{
+	t_color color;
+
+	color.R = 0;
+	color.G = 255;
+	color.B = 0;
+	return (color);
+}
+
+t_color	ft_blue()
+{
+	t_color color;
+
+	color.R = 0;
+	color.G = 0;
+	color.B = 255;
+	return (color);
+}
+
+t_color	ft_white()
+{
+	t_color color;
+
+	color.R = 255;
+	color.G = 255;
+	color.B = 255;
+	return (color);
+}
+
+t_color	ft_yellow()
+{
+	t_color color;
+
+	color.R = 255;
+	color.G = 255;
+	color.B = 0;
+	return (color);
+}
---
+++ utils/ft_split.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split_whitespaces.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/02 01:53:44 by marvin            #+#    #+#             */
+/*   Updated: 2019/07/02 01:53:44 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void ft_fill(char *tab, char *str, int n)
+{
+    static int i;
+    int j;
+
+    j=0;
+    while  (str[i] == '\n' || str[i] == '\t' || str[i] == ' ' && str[i] != '\0')
+        i++;
+    while (str[i] != '\0' && j <= n)
+    {    
+        if (str[i] == '\n' || str[i] == '\t' || str[i] == ' ' && str[i] != '\0')
+            i++;
+        else
+        {
+            tab[j] = str[i++];
+            if (j < n)    
+                j++;
+            if (j >= n)
+                return;
+        }
+    }    
+}
+
+int ft_count_letters(char *str)
+{
+    static int i;
+    int j;
+    j=0;
+    while  (str[i] == '\n' || str[i] == '\t' || str[i] == ' ' && str[i] != '\0')
+        i++;
+    while (str[i] != '\n' && str[i] != '\t' && str[i] != ' ' && str[i] != '\0')
+    {   
+        j++;
+        i++;
+    }
+    while  (str[i] == '\n' || str[i] == '\t' || str[i] == ' ' && str[i] != '\0')
+        i++;
+    return(j);
+}
+
+void ft_skip_whitespaces(char *str, int *j)
+{
+    int i;
+    
+    i=0;
+    while (str[i])
+    {
+        if (str[i] != '\n' && str[i] != '\t' && str[i] != ' ' && str[i] != '\0') 
+            *j = *j + 1;
+        while (str[i] != '\n' && str[i] != '\t' && str[i] != ' '  && str[i] != '\0')
+            i++;
+        while (str[i] == '\n' || str[i] == '\t' || str[i] == ' ')
+            i++;
+    }
+}
+
+char **ft_split(char *str)
+{
+    int i;
+    int j;
+    int h;
+    char **tab;
+
+    i=0;
+    ft_skip_whitespaces(str, &i);
+    h=0;
+    j=-1;
+    if (!(tab = (char**)malloc(sizeof(char*) * (i+1))))
+		return (NULL);
+    tab[i] = '\0';
+    while (++j < i)
+    {
+        h = ft_count_letters(str);
+        if (!(tab[j] = (char*)malloc(sizeof(char) * (h+1))))
+		    return (NULL);
+        tab[j][h] = '\0';
+        ft_fill(tab[j], str, h);
+    }
+    return(tab);
+}
